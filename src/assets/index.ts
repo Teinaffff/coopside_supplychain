6623589b706a598ed86291fd33db44015ef8e501
Beharudin Mohammed <beharudinmm@gmail.com>
2025-01-20 10:14:14 +0300

    Added not found page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,13 +3,15 @@ import coopLogoNoBg from "./images/coop-logo-no-bg.png";
 import coopLogo from "./images/coop.png";
 import loginPageBg from "./images/login-page-bg.png";
 import heroRectangle from "./images/hero-rectangle.png";
+import notFound from "./images/404-page-not-found.svg";
 
 const IMAGES = {
   cmsLogo,
   coopLogo,
   coopLogoNoBg,
   loginPageBg,
-  heroRectangle
+  heroRectangle,
+  notFound,
 };
 
 export { IMAGES };
